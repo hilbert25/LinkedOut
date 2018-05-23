007c02a31af955d5df451261f3f4af2fb1021538
haohaotian <haohaotian@xiaomi.com>
2018-05-23 19:00:11 +0800

    add company auth

--- libs/util.js
+++ libs/util.js
@@ -1,10 +1,12 @@
 var TYPE = "-type";
 var PWD = "-pwd";
 var RESUME = "-resume";
-var USER = "";
-var COMPANY = "";
+var COMPANY_LIST = "company_list";
+var COMPANY_AUTH_LIST = "company_auth_list_";
+var SCHOOL_LIST = "school_list";
+var SCHOOL_AUTH_LIST = "school_auth_list";
 function getContractAddress(){
-    return "n1pFAiC6ceTQq3EQ31Z3jvtR3v6xDieWjKF";
+    return "n1zY8zmyPH2N6vyydUwaqyWG2MAcH6kiqLJ";
 }
 
 function dicToMyJSON(dict){
