99d3b3a1894ee52ee64972703fd31e87d5bae2f5
haohaotian <haohaotian@xiaomi.com>
2018-05-29 18:16:11 +0800

    add

--- libs/util.js
+++ libs/util.js
@@ -7,6 +7,9 @@ var COMPANY_REJECT_LIST = "company_reject_list_";
 var COMPANY_ACCEPTED_LIST = "company_ACCEPTED_list_";
 var SCHOOL_LIST = "school_list";
 var SCHOOL_AUTH_LIST = "school_auth_list";
+var BASIC_INFO = "basic_info";
+var WORK_EXP = "work_exp";
+var EDU_EXP = "edu_exp";
 function getContractAddress(){
     return "n1zY8zmyPH2N6vyydUwaqyWG2MAcH6kiqLJ";
 }
@@ -16,5 +19,8 @@ function dicToMyJSON(dict){
 }
 
 function myJSONParse(data){
-    return JSON.parse(data.replace(/'/g,'"'));
+    if(data!=null)
+        return JSON.parse(data.replace(/'/g,'"'));
+    else
+        return data;
 }
